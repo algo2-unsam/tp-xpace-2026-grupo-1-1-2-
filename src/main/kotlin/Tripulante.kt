2a90a586dc0bc70b225244489ab17933c0194b07
Bruno Coria <brunocoria55@gmail.com>
2026-03-28 20:20:48 -0300

    En proceso

--- src/main/kotlin/Tripulante.kt
+++ src/main/kotlin/Tripulante.kt
@@ -1,17 +1,25 @@
 package ar.edu.unsam.algo2
 
-abstract class Tripulante { // De esta clase heredan los roles
+class Mision() {
+
+}
+
+abstract class Tripulante (var salario_base: Double){ // De esta clase heredan los roles
     // var perfil_aptitud : Perfil_aptitud = COMPOSICION
-    fun salario() : Double {TODO("Not yet implemented")}
+    fun salario() : Double {return salario_base + bonus_salario()}
     abstract fun bonus_salario() : Double
+    fun experiencia():Double {return TODO()}
+    var mision_asignada : Mision
+
+    fun es_apto():Boolean {return experiencia()>3 && !mision_asignada.en_curso() && condiciones_aptitud()}
 }
 
-class Comandante : Tripulante() {
-    override fun bonus_salario(): Double { TODO("Not yet implemented") }
+class Comandante(salario_base: Double) : Tripulante(salario_base) {
+    override fun bonus_salario(): Double {super.salario_base + (super.salario_base*0.05)*super.misiones_exitosas()}
 }
 
-class Piloto() : Tripulante() {
-    override fun bonus_salario(): Double { TODO("Not yet implemented") }
+class Piloto(salario_base: Double) : Tripulante(salario_base) {
+    override fun bonus_salario(): Double {super.salario_base * 0.3}
 }
 
 class Ingeniero() : Tripulante() {
@@ -28,4 +36,32 @@ class Medico() : Tripulante() {
 
 abstract class Perfil_aptitud() { // O podria ser una interfaz, no lo se
     open fun cumple_conidicion_perfil() : Boolean {TODO("Not yet implemented")}
+}
+
+open class Aptitud() {
+    abstract class cumple_condiciones()
+}
+
+class Conformista() : Aptitud() {
+    fun cumple_condiciones() : Boolean {return true}
+}
+
+class Prudente : Aptitud() {
+    fun cumple_condiciones(): Boolean {return planeta_asignado.temperatura_ideal() && planeta_asignado.gravedad_soportable()}
+}
+
+class Explorador() : Aptitud() {
+    fun cumple_condiciones(): Boolean {return planeta_asignado.fue_aterrizado()}
+}
+
+class Veterano() : Aptitud() {
+    fun cumple_condiciones(): Boolean {mision_asignada.duracion_estimada() <= maximo_dias}
+}
+
+class Cauteloso() : Aptitud() {
+    fun cumple_condiciones() : Boolean {planeta_asignado.radiacion() < umbral}
+}
+
+class Exigente() : Aptitud() {
+    fun cumple_condiciones() : Boolean {nave_asignada.es_moderna()}
 }

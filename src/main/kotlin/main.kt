106d58dfada4f88807fdd790143a3983969ca98d
Vickens <victori4pennisi@gmail.com>
2026-03-31 12:56:01 -0300

    Listo planeta y naves falta conectar tripulantes y base

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,5 @@
 package ar.edu.unsam.algo2
+import java.awt.Point
 import java.time.LocalDate
 import java.time.Period
 
@@ -43,8 +44,8 @@ abstract class Planetas {
     open val gravedad: Int = 0
     open val nivelRad: Int = 0
     open val aguaLiquida: Boolean = true
-    open val toxicidadAtmos: Int = 0
-    open val actTectonica: Int = 0
+    open val toxicidadAtmos: Double = 0.0
+    open val actTectonica: Double = 0.0
     open val tamano: Int = 0
     open val fechaDesc: LocalDate = LocalDate.now()
     open val distTierra: Int = 0
@@ -58,6 +59,30 @@ abstract class Planetas {
         return gravedad in 3..15
     }
 
+    fun pocaToxicidad(): Boolean {
+        return toxicidadAtmos < 30
+    }
+
+    fun pocaRad(): Boolean {
+        return nivelRad <40
+    }
+
+    fun tieneAguaLiquida(): Boolean {
+        return aguaLiquida
+    }
+
+    fun esHabitable(): Boolean {
+        return pocaRad() && pocaToxicidad() && gravSoportable() && tempIdeal() && tieneAguaLiquida()
+    }
+
+    fun indicePeligrosidad() : Double {
+        return (nivelRad + toxicidadAtmos+ actTectonica ) /3
+    }
+
+    fun esExplorable(): Boolean {
+        return (indicePeligrosidad() < 60 && ! esHabitable())
+    }
+
 }
 class Pluton : Planetas() {
     override val nombre: String = "Pluton"
@@ -65,8 +90,8 @@ class Pluton : Planetas() {
     override val gravedad: Int = 6
     override val nivelRad: Int = 10
     override val aguaLiquida: Boolean = true
-    override val toxicidadAtmos: Int = 5
-    override val actTectonica: Int = 5
+    override val toxicidadAtmos: Double = 5.0
+    override val actTectonica: Double = 5.0
     override val tamano: Int = 10
     override val fechaDesc: LocalDate = LocalDate.of(2020, 1, 1)
     override val distTierra: Int = 5
@@ -74,14 +99,91 @@ class Pluton : Planetas() {
     override val temperatura: Int = 5
 }
 
-abstract class Naves
+abstract class Naves(
+    val nombre: String,
+    val codigo: String,
+    val fechaFabrica: Int,
+    val velocidad: Int,
+    val autonomia: Int,
+    val consumoNave: Double,
+){
 
-class Sonda : Naves()
+    abstract fun consumoEspecifico(): Double
+    val antiguedad = 2026 - fechaFabrica
+    fun consumoCombu(distanciaPlaneta : Int): Double{
+        return consumoEspecifico() * distanciaPlaneta
+    }
 
-class Transbordador : Naves()
+    //fun enMision(): Boolean { ]//ver essto con los chicos
 
-class Carguero : Naves()
+    fun puedeAlcanzar (distanciaPlaneta : Int): Boolean {
+        return ((distanciaPlaneta *365)/velocidad*2) <= autonomia
+    }
+
+}
+
+class Sonda(
+    nombre: String,
+    codigo: String,
+    fechaFabrica: Int,
+    velocidad: Int,
+    autonomia: Int,
+    consumoNave: Double
+) : Naves(nombre, codigo, fechaFabrica, velocidad, autonomia, consumoNave) {
+    override fun consumoEspecifico(): Double {
+        return consumoNave
+    }
+}
+
+class Transbordador(
+    nombre: String,
+    codigo: String,
+    fechaFabrica: Int,
+    velocidad: Int,
+    autonomia: Int,
+    consumoNave: Double,
+    val numPasajeros: Int,
+) : Naves(nombre, codigo, fechaFabrica, velocidad, autonomia, consumoNave){
+    override fun consumoEspecifico(): Double {
+        return consumoNave + ((consumoNave *0.1) * numPasajeros)
+    }
+}
+
+class Carguero(
+    nombre: String,
+    codigo: String,
+    fechaFabrica: Int,
+    velocidad: Int,
+    autonomia: Int,
+    consumoNave: Double,
+    val capacidadCarga: Double
+) : Naves(nombre, codigo, fechaFabrica, velocidad, autonomia, consumoNave){
+    override fun consumoEspecifico(): Double {
+        return if (antiguedad < 10){
+            consumoNave + (consumoNave * 0.05 * capacidadCarga)
+        } else{
+            (consumoNave + (consumoNave * 0.05 * capacidadCarga)) *1.2
+        }
+    }
+}
 
 abstract class Misiones
 
 class Mision1 : Misiones()
+
+class BaseLanzamiento(
+    val nombre: String,
+    val maximoNaves: Int,
+    val ubicacion : Direccion
+){
+    val NavesEstacionadas = listOf<Naves>()
+
+}
+
+class Direccion(
+    val pais : String,
+    val Ciudad : String,
+    val calle : String,
+    val altura : Int,
+    val ubiGeo : Point
+){}
